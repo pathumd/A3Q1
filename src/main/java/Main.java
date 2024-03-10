861d2d87f21cc89ad820cb18f61e0db659406231
Pathum Danthanarayana <pathumkavindya@gmail.com>
2024-03-10 14:38:54 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main class
+ *
+ * @author Pathum Danthanarayana, 101181411
+ * @date March 10, 2024
+ */
+public class Main {
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+
+    }
+}
